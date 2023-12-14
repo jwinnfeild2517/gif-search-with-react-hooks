dd02df7842a7c1a090f88030d436d8b846472060
Jenner Thomas <jennerthomas@Jenners-Air.attlocal.net>
2023-12-13 20:16:50 -0500

    gif search

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { GifList } from "./components/Gif";
+import SearchForm from "./components/SearchForm";
+import './App.css'
+
+const GIF_KEY = process.env.REACT_APP_API_KEY
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("the office");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const performSearch = (value) => setQuery(value);
+
+  const getData = useCallback(async () => {
+    try {
+      const res = await fetch(
+        `http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=${GIF_KEY}`
+      );
+      const resj = await res.json()
+
+      setData(resj.data);
+    } catch (error) {
+      console.log("error fetching gif data", error);
+    }
+    setIsLoading(false)
+  }, [query]);
+
+  useEffect(() => {
+    getData()
+  }, [query, getData])
+
+  return <>
+  <div className="main-header">
+    <div className="inner">
+      <h1 className="main-title">GifSearch</h1>
+      <SearchForm onSearch={performSearch}/>
+    </div>
+  </div>
+  <div className="main-content">
+    {
+      isLoading ? <p>Loading...</p> : <GifList data={data} />
+    }
+  </div>
+</>;
+};
+
+export default App;
--- src/Components/Gif.js
+++ src/Components/Gif.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {} from 'react'
 
-const Gif = props => (
-  <li className="gif-wrap">
-    <a href={props.url} target="_blank">
+export const Gif = (props) => (
+    <li className="gif-wrap">
+    <a href={props.url} target="_blank" rel='noreferrer'>
       <img src={props.url} alt=""/>
     </a>
   </li>
-);
+)
 
-export default Gif;
+export const GifList = props => { 
+    const results = props.data;
+    let gifs;
+    if (results && results.length > 1) {
+      gifs = results.map(gif => <Gif url={gif.images.fixed_height.url} key={gif.id} />);    
+    } else {
+      gifs = <></>
+    }
+  
+    return(
+      <ul className='gif-list'>
+        {gifs}
+      </ul> 
+    );
+  }
+  
+  export default GifList;
